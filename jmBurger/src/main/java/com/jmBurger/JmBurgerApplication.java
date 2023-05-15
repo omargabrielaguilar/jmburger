28e0ac62698a622a27d620eccc236328f5248e46
Omar <saucyfunnel@gmail.com>
2023-05-15 16:15:59 -0500

    proyecto crud example

---
+++ jmBurger/src/main/java/com/jmBurger/JmBurgerApplication.java
@@ -0,0 +1,13 @@
+package com.jmBurger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmBurgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmBurgerApplication.class, args);
+	}
+
+}
---
+++ jmBurger/src/test/java/com/jmBurger/JmBurgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmBurger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmBurgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
