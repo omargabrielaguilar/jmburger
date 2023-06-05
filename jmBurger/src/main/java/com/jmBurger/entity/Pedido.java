ff04f620a283d8b21982fe2dfc2b806eb06c7ba1
omargabrielaguilar <saucyfunnel@gmail.com>
2023-06-05 11:42:25 -0500

    fin entidad pedido

--- jmBurger/src/main/java/com/jmBurger/entity/Pedido.java
+++ jmBurger/src/main/java/com/jmBurger/entity/Pedido.java
@@ -1,17 +1,84 @@
 package com.jmBurger.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "pedido")
 public class Pedido {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private int idPedido;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "fecha_pedido")
+    private Date fechaPedido;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado_pedido")
+    private EstadoPedido estadoPedido;
+
+    @Column (name = "comentarios")
+    private String comentarios;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    public enum EstadoPedido {
+        PENDIENTE,
+        EN_EJECUCIÓN,
+        ENTREGADO
+    }
+
+    public Pedido(){}
+
+    public Pedido(int idPedido, Date fechaPedido, EstadoPedido estadoPedido, String comentarios, Usuario usuario) {
+        this.idPedido = idPedido;
+        this.fechaPedido = fechaPedido;
+        this.estadoPedido = estadoPedido;
+        this.comentarios = comentarios;
+        this.usuario = usuario;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public Date getFechaPedido() {
+        return fechaPedido;
+    }
+
+    public void setFechaPedido(Date fechaPedido) {
+        this.fechaPedido = fechaPedido;
+    }
+
+    public EstadoPedido getEstadoPedido() {
+        return estadoPedido;
+    }
+
+    public void setEstadoPedido(EstadoPedido estadoPedido) {
+        this.estadoPedido = estadoPedido;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
