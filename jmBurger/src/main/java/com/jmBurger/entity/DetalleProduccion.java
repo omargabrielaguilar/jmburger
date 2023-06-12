59ae054bd95a0f434743d29f7ef1aa6cfd1f92eb
omargabrielaguilar <saucyfunnel@gmail.com>
2023-06-12 16:36:24 -0500

    detalle prod entity

--- jmBurger/src/main/java/com/jmBurger/entity/DetalleProduccion.java
+++ jmBurger/src/main/java/com/jmBurger/entity/DetalleProduccion.java
@@ -1,4 +1,22 @@
 package com.jmBurger.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "detalle_produccion")
 public class DetalleProduccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_detalle_produccion")
+    private int idDetalleProduccion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_produccion")
+    private Produccion produccion;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto")
+    private Producto producto;
+
+
 }
