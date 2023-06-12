fc2be332ff5d65629dbf48c765e7809304843e74
omargabrielaguilar <saucyfunnel@gmail.com>
2023-06-12 14:31:19 -0500

    entidad creada

--- jmBurger/src/main/java/com/jmBurger/entity/DetallePedido.java
+++ jmBurger/src/main/java/com/jmBurger/entity/DetallePedido.java
@@ -1,12 +1,103 @@
 package com.jmBurger.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.util.Date;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name="detalle_pedido")
+@Table(name = "detalle_pedido")
 public class DetallePedido {
+
     @Id
-    @
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_detalle_pedido")
+    private int idDetallePedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto")
+    private Producto producto;
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    @Column(name = "precio_unitario")
+    private double precioUnitario;
+
+    @Column(name = "precio_total")
+    private double precioTotal;
+
+    @Column(name = "fecharegistro")
+    private Date fechaRegistro;
+
+    // Constructor, getters, and setters
+    public DetallePedido(){}
+
+    public DetallePedido(int idDetallePedido, Pedido pedido, Producto producto, int cantidad, double precioUnitario, double precioTotal, Date fechaRegistro) {
+        this.idDetallePedido = idDetallePedido;
+        this.pedido = pedido;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.precioTotal = precioTotal;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getIdDetallePedido() {
+        return idDetallePedido;
+    }
+
+    public void setIdDetallePedido(int idDetallePedido) {
+        this.idDetallePedido = idDetallePedido;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public double getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public void setPrecioTotal(double precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
 }
