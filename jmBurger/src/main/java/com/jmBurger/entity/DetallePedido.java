f3b5c57f7efb39a784dad4642055b653ad8de27d
omargabrielaguilar <saucyfunnel@gmail.com>
2023-06-12 14:21:22 -0500

    trabajar con detallepedido

--- jmBurger/src/main/java/com/jmBurger/entity/DetallePedido.java
+++ jmBurger/src/main/java/com/jmBurger/entity/DetallePedido.java
@@ -1,4 +1,12 @@
 package com.jmBurger.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="detalle_pedido")
 public class DetallePedido {
+    @Id
+    @
 }
