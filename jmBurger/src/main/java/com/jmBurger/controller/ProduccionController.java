bfe5bd1c80c75b42fcef9180e750c5ca4524dfb6
omargabrielaguilar <saucyfunnel@gmail.com>
2023-06-13 10:42:26 -0500

    avanzanding

--- jmBurger/src/main/java/com/jmBurger/controller/ProduccionController.java
+++ jmBurger/src/main/java/com/jmBurger/controller/ProduccionController.java
@@ -1,6 +1,9 @@
 package com.jmBurger.controller;
 
+import com.jmBurger.entity.Pedido;
 import com.jmBurger.entity.Produccion;
+import com.jmBurger.entity.Producto;
+import com.jmBurger.entity.Proveedor;
 import com.jmBurger.repository.PedidoRepository;
 import com.jmBurger.repository.ProduccionRepository;
 import com.jmBurger.repository.ProductoRepository;
@@ -8,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProduccionController {
@@ -35,5 +41,43 @@ public class ProduccionController {
         return "nuevaProduccion";
     }
 
+    @PostMapping("produccion/nuevo")
+    public String crearProduccion(@ModelAttribute("produccion") Produccion produccion){
+        Producto producto = productoRepository.findByNombreProducto(produccion.getProducto().getNombreProducto());
+        Pedido pedido = pedidoRepository.findByFechaPedido(produccion.getPedido().getFechaPedido());
+
+        if(pedido == null){
+            pedido = new Pedido();
+            pedido.setFechaPedido(produccion.getPedido().getFechaPedido());
+            pedidoRepository.save(pedido);
+        }
+
+        if(producto == null){
+            producto = new Producto();
+            producto.setNombreProducto(produccion.getProducto().getNombreProducto());
+            productoRepository.save(producto);
+        }
+
+        produccion.setPedido(pedido);
+        produccion.setProducto(producto);
+        produccionRepository.save(produccion);
+        return "redirect:/produccion";
+    }
+
+    @GetMapping("produccion/{id}/editar")
+    public String mostrarFormularioEditar(@PathVariable("id") int id, Model model){
+        Produccion produccion = produccionRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Pedido no encontrado con ID: " + id));
+
+        model.addAttribute("produccion", produccion);
+        model.addAttribute("pedido", pedidoRepository.findAll());
+        model.addAttribute("producto", productoRepository.findAll());
+
+        return "editarProduccion";
+    }
+
+    @PostMapping("produccion/{id}/editar")
+    public String actualizarProduccion(@PathVariable("id") int id, @ModelAttribute("produccion") Produccion produccion){
+        return "redirect:/produccion";
+    }
 
 }
