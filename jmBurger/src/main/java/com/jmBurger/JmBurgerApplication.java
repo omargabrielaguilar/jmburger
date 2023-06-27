0839357f9cb8dc7091652d567d5844614f79410d
Omar <saucyfunnel@gmail.com>
2023-06-27 17:17:52 -0500

    hubo confusión

--- jmBurger/src/main/java/com/jmBurger/JmBurgerApplication.java
+++ jmBurger/src/main/java/com/jmBurger/JmBurgerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import com.jmBurger.config.SecurityConfig;
+//import com.jmBurger.config.SecurityConfig;
 
 @SpringBootApplication
-@Import(SecurityConfig.class) // Agrega esta línea para importar la clase SecurityConfig
+//@Import(SecurityConfig.class) // Agrega esta línea para importar la clase SecurityConfig
 public class JmBurgerApplication {
     public static void main(String[] args) {
         SpringApplication.run(JmBurgerApplication.class, args);
--- jmBurger/src/main/java/com/jmBurger/controller/LoginController.java
+++ jmBurger/src/main/java/com/jmBurger/controller/LoginController.java
@@ -1,28 +1,52 @@
+/*
+
 package com.jmBurger.controller;
 
+import com.jmBurger.entity.Usuario;
+import com.jmBurger.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public LoginController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @GetMapping("/login")
     public String showLoginForm() {
-        return "login"; // Nombre del archivo HTML del formulario de inicio de sesión
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam("nombreUsuario") String nombreUsuario, @RequestParam("contraseniaHash") String contraseniaHash, Model model) {
+
+        // Verificar las credenciales del usuario
+        Usuario usuario = usuarioRepository.findByNombreUsuario(nombreUsuario);
+        if (usuario != null && nombreUsuario.equals(usuario.getNombreUsuario()) && contraseniaHash.equals(usuario.getContraseniaHash())) {
+            System.out.println("Credenciales válidas. Redireccionando al dashboard");
+            // Credenciales válidas, redirigir al dashboard
+            return "redirect:/dashboard";
+        } else {
+            System.out.println("Credenciales inválidas. Mostrando mensaje de error");
+            // Credenciales inválidas, mostrar un mensaje de error en la página de inicio de sesión
+            model.addAttribute("error", "Credenciales inválidas");
+            return "login";
+        }
     }
 
     @GetMapping("/dashboard")
     public String showDashboard() {
         return "dashboard";
     }
-
-    @PostMapping("/login")
-    public String postLogin() {
-        // Realiza cualquier lógica adicional de autenticación o verificación aquí
-        // ...
-
-        return "redirect:/dashboard"; // Redirige al usuario a la página de dashboard
-    }
 }
+
+*/
