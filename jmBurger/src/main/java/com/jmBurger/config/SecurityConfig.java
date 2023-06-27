343c88aaf8fd63f7f9f1919010d909efaa504832
Omar <saucyfunnel@gmail.com>
2023-06-27 08:58:13 -0500

    mitad de sesiones

--- jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
+++ jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jmBurger.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(usuarioSesiones)
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/**").hasRole("administrador")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login") // Especifica la URL de la página de inicio de sesión
+                .defaultSuccessUrl("/dashboard") // Especifica la URL de éxito después de iniciar sesión
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
 
     // Otras configuraciones de seguridad, como autorizaciones y configuraciones de CORS, CSRF, etc.
 }
