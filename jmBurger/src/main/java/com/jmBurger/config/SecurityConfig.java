260273023d539c3897b500d5664f19121bddd2aa
Omar <saucyfunnel@gmail.com>
2023-06-27 11:02:33 -0500

    listooo

--- jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
+++ jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
@@ -1,45 +1,57 @@
 package com.jmBurger.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.jmBurger.entity.UsuarioSesiones;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+    
+    private UserDetailsService userDetailsService;
 
-    @Autowired
-    private UsuarioSesiones usuarioSesiones;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(usuarioSesiones)
-                .passwordEncoder(new BCryptPasswordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .antMatchers("/**").hasRole("administrador")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login") // Especifica la URL de la página de inicio de sesión
-                .defaultSuccessUrl("/dashboard") // Especifica la URL de éxito después de iniciar sesión
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .authorizeRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers("/**").hasRole("administrador")
+                                .anyRequest().authenticated()
+                )
+                .formLogin((formLogin) ->
+                        formLogin
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/dashboard")
+                                .permitAll()
+                )
+                .logout((logout) ->
+                        logout
+                                .permitAll()
+                );
+
+        return http.build();
     }
 
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
 
-    // Otras configuraciones de seguridad, como autorizaciones y configuraciones de CORS, CSRF, etc.
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return userDetailsService;
+    }
 }
--- jmBurger/src/main/java/com/jmBurger/controller/LoginController.java
+++ jmBurger/src/main/java/com/jmBurger/controller/LoginController.java
@@ -1,4 +1,18 @@
 package com.jmBurger.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login"; // Nombre del archivo HTML del formulario de inicio de sesión
+    }
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "dashboard"; // Nombre de la vista del panel de control después de iniciar sesión
+    }
 }
