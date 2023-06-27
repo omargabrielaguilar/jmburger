b554ee9fac5abeb7be147fbaa4f5ceac185304ad
Omar <saucyfunnel@gmail.com>
2023-06-27 13:06:52 -0500

    fin sesiones, pre boostrap

--- jmBurger/src/main/java/com/jmBurger/JmBurgerApplication.java
+++ jmBurger/src/main/java/com/jmBurger/JmBurgerApplication.java
@@ -2,8 +2,12 @@ package com.jmBurger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.jmBurger.config.SecurityConfig;
 
 @SpringBootApplication
+@Import(SecurityConfig.class) // Agrega esta línea para importar la clase SecurityConfig
 public class JmBurgerApplication {
     public static void main(String[] args) {
         SpringApplication.run(JmBurgerApplication.class, args);
--- jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
+++ jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
@@ -1,36 +1,50 @@
 package com.jmBurger.config;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
-    
+
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserDetailsService(@Qualifier("usuarioSesiones") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
                 .authorizeRequests((authorizeRequests) ->
                         authorizeRequests
                                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                                 .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .requestMatchers("/**").hasRole("administrador")
+                                .requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
                 )
+
                 .formLogin((formLogin) ->
                         formLogin
                                 .loginPage("/login")
-                                .defaultSuccessUrl("/dashboard")
+                                .defaultSuccessUrl("/dashboard", true)
                                 .permitAll()
                 )
                 .logout((logout) ->
@@ -41,17 +55,16 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Bean
     public UserDetailsService userDetailsServiceBean() throws Exception {
         return userDetailsService;
     }
+
 }
+
