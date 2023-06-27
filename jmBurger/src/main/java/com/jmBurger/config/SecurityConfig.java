7f0085c5b35872a4ff2c9692a94a552049a945ff
Omar <saucyfunnel@gmail.com>
2023-06-27 15:04:40 -0500

    fin? comenzr con boostrap

--- jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
+++ jmBurger/src/main/java/com/jmBurger/config/SecurityConfig.java
@@ -1,24 +1,21 @@
 package com.jmBurger.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityConfig {
+public class SecurityConfig  {
 
     private UserDetailsService userDetailsService;
 
@@ -30,34 +27,30 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .requestMatchers("/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                /*
-                .formLogin((formLogin) ->
-                        formLogin
-                                .loginPage("/login")
-                                .defaultSuccessUrl("/dashboard")
-                                .permitAll()
-                )
-                */
-                .logout((logout) ->
-                        logout
-                                .permitAll()
-                );
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/login").permitAll() // Permitir acceso sin autenticación a /login
+                .requestMatchers(HttpMethod.GET).permitAll() // Permitir acceso sin autenticación a solicitudes GET
+                .anyRequest().authenticated() // Requiere autenticación para todas las demás solicitudes
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/dashboard", true) // Redireccionar a /dashboard después del inicio de sesión
+                .permitAll()
+                .and()
+                .httpBasic();
 
         return http.build();
     }
+
+
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
+
 
     @Bean
     public UserDetailsService userDetailsServiceBean() throws Exception {
